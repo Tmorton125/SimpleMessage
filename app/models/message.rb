8dc7b65f5e189146f5ba01dfb240e3c8ca7ae5c3
Tmorton125 <tmorton125>
2015-01-18 13:57:47 -0500

    Adding Simple Validations
    Adding Some Simple validations to Message Model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,9 @@ class Message < ActiveRecord::Base
 	# :creator_id
 	# :delivery_time
 	# :is_disabled
+
+	validates :name, presence: true, 
+		uniqueness: { scope: :creator_id }
+		
+	validates :content, length : { maximum: 255 }
 end
